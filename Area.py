ef7dbd57448e563b7e6b725511af20991d9f0b48
Ankit-Kumar9 <86050101+Ankit-Kumar9@users.noreply.github.com>
2021-08-11 07:27:55 +0530

    Create_Area.py

---
+++ Area.py
@@ -0,0 +1,3 @@
+#To print the area of circle whose radius is accepted
+r = float(input ("Input the radius of the circle : ")) 
+print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2)) 
